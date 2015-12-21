e97c239e6fbcc2200b7b541d0afc43d85ca3c2ad
surikat <jo@surikat.pro>
2015-12-21 23:54:16 +0100

    install:end

---
+++ Artist/InstallEnd.php
@@ -0,0 +1,21 @@
+<?php
+namespace RedCat\Framework\Artist;
+class InstallEnd extends Artist{
+	protected $description = "Finalize installation";
+	protected $args = [];
+	protected $opts = [];
+	protected function exec(){
+		if(!is_file($this->cwd.'vendor/.htaccess')&&file_put_contents($this->cwd.'vendor/.htaccess','Deny from All'))
+			$this->output->writeln('vendor dir protected');
+		if(!is_dir($this->cwd.'.tmp')){
+			if(mkdir($this->cwd.'.tmp')){
+				$this->output->writeln('.tmp created');
+			}
+			else{
+				$this->output->writeln('.tmp creation failed');
+			}
+		}
+		if(is_file($this->cwd.'.tmp')&&chmod($this->cwd.'.tmp',0777))
+			$this->output->writeln('.tmp to chmod 0777');
+	}
+}
