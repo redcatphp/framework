f469bf00b28f6ca304aa96d597d02c6cebb0ca25
surikat <jo@surikat.pro>
2015-12-28 23:49:50 +0100

    update

--- Artist/InstallEnd.php
+++ Artist/InstallEnd.php
@@ -5,8 +5,8 @@ class InstallEnd extends Artist{
 	protected $args = [];
 	protected $opts = [];
 	protected function exec(){
-		if(!is_file($this->cwd.'vendor/.htaccess')&&file_put_contents($this->cwd.'vendor/.htaccess','Deny from All'))
-			$this->output->writeln('vendor dir protected');
+		if(!is_file($this->cwd.'packages/.htaccess')&&file_put_contents($this->cwd.'packages/.htaccess','Deny from All'))
+			$this->output->writeln('packages dir protected');
 		if(!is_dir($this->cwd.'.tmp')){
 			if(mkdir($this->cwd.'.tmp')){
 				$this->output->writeln('.tmp created');
