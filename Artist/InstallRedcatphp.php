a2491c0b99fc2aa3262f40fbd79b3f6f273be104
surikat <jo@surikat.pro>
2016-05-26 12:35:21 +0200

    add install:redcatphp to artist cli

---
+++ Artist/InstallRedcatphp.php
@@ -0,0 +1,35 @@
+<?php
+namespace RedCat\Framework\Artist;
+use RecursiveIteratorIterator;
+use RecursiveDirectoryIterator;
+class InstallRedcatphp extends Artist{
+	protected $description = "Install redcatphp package from vendor dir to top level of application";
+	protected $args = [];
+	protected $opts = [];
+	protected function exec(){		
+		if($this->recursiveCopy($this->cwd.'packages/redcatphp/redcatphp',$this->cwd)){
+			$this->output->writeln('redcatphp bootstrap installed');
+		}
+		else{
+			$this->output->writeln('redcatphp bootstrap failed to install');
+		}
+	}
+	private function recursiveCopy($source,$dest){
+		if(!is_dir($dest)){
+			$r = mkdir($dest, 0755);
+			if($r===false) return false;
+		}
+		$rdirectory = new RecursiveDirectoryIterator($source, RecursiveDirectoryIterator::SKIP_DOTS);
+		$iterator = new RecursiveIteratorIterator($rdirectory,RecursiveIteratorIterator::SELF_FIRST);
+		foreach($iterator as $item){
+			if($item->isDir()){
+				$r = mkdir($dest . DIRECTORY_SEPARATOR . $iterator->getSubPathName());
+			}
+			else{
+				$r = copy($item, $dest . DIRECTORY_SEPARATOR . $iterator->getSubPathName());
+			}
+			if($r===false) return false;
+		}
+		return true;
+	}
+}
