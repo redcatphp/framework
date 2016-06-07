158411ed178b1ac79a4bdd236d1a3308f457cdee
surikat <jo@surikat.pro>
2016-06-07 18:07:59 +0200

    fix

--- ComposerScript.php
+++ ComposerScript.php
@@ -3,8 +3,8 @@ namespace RedCat\Framework;
 class ComposerScript{
 	static function __callStatic($func,$args){
 		$c = 'MyApp\\Composer\EventsHandler';
-		if(!class_exists($c)&&is_file('plugins/composer/EventsHandler.php')){
-			include 'plugins/composer/ComposerScript.php';
+		if(!class_exists($c)&&is_file($inc='plugins/composer/EventsHandler.php')){
+			include $inc;
 		}
 		if(class_exists($c)&&is_callable([$c,$func])){
 			call_user_func_array([$c,$func],$args);
