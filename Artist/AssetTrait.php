db7d8c9edf2964fc71b1c6d655d08211b9b4ef18
surikat <jo@surikat.pro>
2016-06-19 22:55:27 +0200

    bugfix

--- Artist/AssetTrait.php
+++ Artist/AssetTrait.php
@@ -4,8 +4,8 @@ trait AssetTrait{
 	protected $bowerAssetDir = 'vendors/bower-asset';
 	protected $npmAssetDir = 'vendors/npm-asset';
 	function loadAssetInstallerPaths(){
-		if(is_file($this->cwd.'composer.json')){
-			$json = json_decode(file_get_contents($this->cwd.'composer.json'),true);
+		if(is_file('composer.json')){
+			$json = json_decode(file_get_contents('composer.json'),true);
 			if(is_array($json)){
 				if(isset($json['extra']['asset-installer-paths']['bower-asset-library'])){
 					$this->bowerAssetDir = $json['extra']['asset-installer-paths']['bower-asset-library'];
