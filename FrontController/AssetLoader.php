4ea62babc5acebf70b45bcd29f17abc032be85d9
jo <jo@surikat.pro>
2016-11-16 00:27:04 +0100

    update

--- FrontController/AssetLoader.php
+++ FrontController/AssetLoader.php
@@ -3,10 +3,11 @@ namespace RedCat\Framework\FrontController;
 use RedCat\Route\Router;
 use RedCat\Strategy\Di;
 use RedCat\Stylize\Server as StylizeServer;
-use RedCat\Artist\Plugin\AssetTrait;
 use JShrink\Minifier as JSMin;
 class AssetLoader {
-	use AssetTrait;
+	protected $bowerAssetDir = 'vendor/bower-asset';
+	protected $npmAssetDir = 'vendor/npm-asset';
+	
 	protected $pathFS;
 	protected $expires = 2592000;
 	protected $allowedExtensions = ['css','js','jpg','jpeg','png','gif'];
@@ -399,5 +400,24 @@ EOS;
 			}
 		}
 		return $this->devLevel;
+	}
+	
+	function loadAssetInstallerPaths(){
+		$cwd = property_exists($this,'cwd')?$this->cwd:getcwd();
+		if(is_file($cwd.'composer.json')){
+			$json = json_decode(file_get_contents($cwd.'composer.json'),true);
+			if(is_array($json)){
+				if(isset($json['config']['vendor-dir'])){
+					$this->bowerAssetDir = $json['config']['vendor-dir'].'/bower-asset';
+					$this->npmAssetDir = $json['config']['vendor-dir'].'/npm-asset';
+				}
+				if(isset($json['extra']['asset-installer-paths']['bower-asset-library'])){
+					$this->bowerAssetDir = $json['extra']['asset-installer-paths']['bower-asset-library'];
+				}
+				if(isset($json['extra']['asset-installer-paths']['npm-asset-library'])){
+					$this->npmAssetDir = $json['extra']['asset-installer-paths']['npm-asset-library'];
+				}
+			}
+		}
 	}
 }
